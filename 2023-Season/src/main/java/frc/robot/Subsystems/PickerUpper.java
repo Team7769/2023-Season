49c146f11e638c574234f69f26d0f38e29ba43c8
Codingwithcn <67172682+codingwithcn@users.noreply.github.com>
2023-02-04 13:56:26 -0500

    added motorrs

--- 2023-Season/src/main/java/frc/robot/Subsystems/PickerUpper.java
+++ 2023-Season/src/main/java/frc/robot/Subsystems/PickerUpper.java
@@ -1,10 +1,28 @@
 package frc.robot.Subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import frc.robot.Configuration.Constants;
+
 public class PickerUpper extends Subsystem {
     private static PickerUpper _instance = null;
 
-    PickerUpper() {
+    private CANSparkMax _leftMotor;
+    private CANSparkMax _rightMotor;
 
+    PickerUpper() {
+        _leftMotor = new CANSparkMax(Constants.kPickerUpperLeftMotorDeviceId, MotorType.kBrushless);
+        _rightMotor = new CANSparkMax(Constants.kPickerUpperRightMotorDeviceId, MotorType.kBrushless);
+
+        _leftMotor.setInverted(true);
+        _leftMotor.setIdleMode(IdleMode.kBrake);
+
+        _rightMotor.setIdleMode(IdleMode.kBrake);
+
+        _leftMotor.follow(_rightMotor);
     }
 
     public static PickerUpper getInstance() {
@@ -24,7 +42,7 @@ public class PickerUpper extends Subsystem {
     }
 
     public void collect() {
-        
+
     }
 
 }
--- 2023-Season/src/main/java/frc/robot/Subsystems/PlacerDowner.java
+++ 2023-Season/src/main/java/frc/robot/Subsystems/PlacerDowner.java
@@ -1,10 +1,28 @@
 package frc.robot.Subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
+
+import frc.robot.Configuration.Constants;
+
 public class PlacerDowner extends Subsystem {
     private static PlacerDowner _instance = null;
 
-    PlacerDowner() {
+    private CANSparkMax _placerDownerMotor;
+    private PIDController _placerDownerPID;
 
+    PlacerDowner() {
+        _placerDownerMotor = new CANSparkMax(Constants.kPlacerDownerMotorDeviceId, MotorType.kBrushless);
+
+        _placerDownerMotor.setInverted(true);
+        _placerDownerMotor.setIdleMode(IdleMode.kBrake);
+
+        _placerDownerPID = new PIDController(Constants.kPlacerDownerKp, Constants.kPlacerDownerKi, Constants.kPlacerDownerKd);
+        _placerDownerPID.setTolerance(0.005);
     }
 
     public static PlacerDowner getInstance() {
@@ -16,9 +34,8 @@ public class PlacerDowner extends Subsystem {
     }
 
     public void  liftUp() {
-
     }
-    
+
     public void placeDown() {
 
     }
