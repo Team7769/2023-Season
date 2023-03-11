b750914fb83231a89d5a0ac7bb3ad1308d57e9c7
Codingwithcn <67172682+codingwithcn@users.noreply.github.com>
2023-03-10 19:41:18 -0500

    led config change

--- 2023-Season/src/main/java/frc/robot/Utilities/LEDController.java
+++ 2023-Season/src/main/java/frc/robot/Utilities/LEDController.java
@@ -32,6 +32,43 @@ public class LEDController {
     public final double kGold = 0.67;
     public final double kStrobeRed = -0.11;
     public final double kStrobeBlue = -0.09;
+    public final double kRainbowPalette = 0.99;
+    public final double kRainbowPartyPalette = -0.97;
+    public final double kRainbowOceanPalette = -0.95;
+    public final double kRainbowLavePalette = -0.93;
+    public final double kRainbowForestPalette = -0.91;
+    public final double kRainbowWithGlitter = -0.89;
+    public final double kShotRed = -0.85;
+    public final double kShotBlue = -0.83;
+    public final double kShotWhite = -0.81;
+    public final double kSinelonRainbowPalette = -0.79;
+    public final double kSinelonPartyPalette = -0.77;
+    public final double kSinelonOceanPalette = -0.75;
+    public final double kSinelonLavaPalette = -0.73;
+    public final double kSinelonForestPalette = -0.71;
+    public final double kBeatsPerMinuteRainbowPalette = -0.69;
+    public final double kBeatsPerMinutePartyPalette = -0.67;
+    public final double kBeatsPerMinuteOceanPalette = -0.65;
+    public final double kBeatsPerMinuteLavaPalette = -0.63;
+    public final double kBeatsPerMinuteForestPalette = -0.61;
+    public final double kFireLarge = -0.57;
+    public final double kTwinklesRainbowPalette = -0.55;
+    public final double kTwinklesPartyPalette = -0.53;
+    public final double kTwinklesOceanPalette = -0.51;
+    public final double kTwinklesLavaPalette = -0.49;
+    public final double kTwinklesForestPalette = -0.47;
+    public final double kColorWavesRainbowPalette = -0.45;
+    public final double kColorWavesPartyPalette = -0.43;
+    public final double kColorWavesOceanPalette = -0.41;
+    public final double kColorWavesLavaPalette = -0.39;
+    public final double kColorWavesForestPalette = -0.37;
+    public final double kLarsonScannerRed = -0.35;
+    public final double kLarsonScannerGray = -0.33;
+    public final double kLightChaseRed = -0.31;
+    public final double kLightChaseBlue = -0.29;
+    public final double kLightChaseGray = -0.27;
+    
+
 
     public LEDController() {
         _bellyPanBlinkin = new Spark(1);
